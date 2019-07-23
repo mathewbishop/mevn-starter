773955538c6addb88c392f6132fea78be74db3c7
mbishop <39043451+mathewbishop@users.noreply.github.com>
2019-07-22 19:32:49 -0500

    Rename route file and add mongoose schema boilerplate.

--- models/model.js
+++ models/model.js
@@ -1,1 +1,12 @@
 // Rename this file and define a model here.
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const placeholder = new Schema({
+
+});
+
+
+const placeholder = mongoose.model("", bookSchema);
+
+module.exports = placeholder;
